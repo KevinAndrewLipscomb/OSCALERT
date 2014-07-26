0f69928a0d3faf7530338ba77742b7aff6a84cf2
kevinanlipscomb <kevinanlipscomb@fcafd3cc-6610-0410-92fd-86a883f3e63b>
2014-07-26 23:17:15 +0000

    Moved field_situation.sql from OscarDotNet to OSCALERT.  In work.bat, generalized the invocation of MySQL Workbench.

--- trunk/OSCALERT/work.bat
+++ trunk/OSCALERT/work.bat
@@ -4,4 +4,4 @@ REM
 cd "C:\Inetpub\wwwroot\OSCALERT"
 start /max explorer /e,/select,C:\Inetpub\wwwroot\OSCALERT\.svn
 start /max OSCALERT.sln
-start "" /max "C:\Program Files\MySQL\MySQL Workbench CE 5.2.47\MySQLWorkbench.exe"
+start "" /max "C:\Program Files\MySQL\MySQL Workbench\MySQLWorkbench.exe"
