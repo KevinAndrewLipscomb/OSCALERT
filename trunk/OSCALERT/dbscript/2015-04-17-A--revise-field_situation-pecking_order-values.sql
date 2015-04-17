f97ab35cc8c3901897ffcbb5ef2618cc0f673ed1
kevinanlipscomb <kevinanlipscomb@fcafd3cc-6610-0410-92fd-86a883f3e63b>
2015-04-17 17:00:29 +0000

    Creation.

---
+++ trunk/OSCALERT/dbscript/2015-04-17-A--revise-field_situation-pecking_order-values.sql
@@ -0,0 +1,21 @@
+START TRANSACTION
+;
+set foreign_key_checks = 0
+;
+UPDATE `field_situation_impression` SET `pecking_order`='3501' WHERE description = 'AmbNeeded' -- frees up 3000
+;
+UPDATE `field_situation_impression` SET `pecking_order`='3000' WHERE description = 'AlsNeeded' -- frees up 3500
+;
+UPDATE `field_situation_impression` SET `pecking_order`='3500' WHERE description = 'AmbNeeded'
+;
+set foreign_key_checks = 1
+;
+update member
+set min_oscalert_peck_order_general = 3500
+where min_oscalert_peck_order_general = 3000
+;
+update member
+set min_oscalert_peck_order_general = 3000
+where min_oscalert_peck_order_general = 3500
+;
+COMMIT
