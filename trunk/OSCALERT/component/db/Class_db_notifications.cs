8cd5498512d87d3cd6e00e8fc64ec43ff99279f7
kevinanlipscomb <kevinanlipscomb@fcafd3cc-6610-0410-92fd-86a883f3e63b>
2015-09-08 14:35:06 +0000

    Reverted this month's changes that did not improve notification difficulties.

--- trunk/OSCALERT/component/db/Class_db_notifications.cs
+++ trunk/OSCALERT/component/db/Class_db_notifications.cs
@@ -1,6 +1,8 @@
 using Class_db;
 using kix;
 using MySql.Data.MySqlClient;
+using System;
+using System.Collections;
 using System.Configuration;
 using System.Web.UI.WebControls;
 
@@ -133,6 +135,49 @@ namespace Class_db_notifications
       // dr.Close();
       Close();
       return (target_of.Length > 0 ? target_of.Substring(0, target_of.Length - 1) : k.EMPTY);
+      }
+
+    internal string TargetOfOscalert(string description)
+      {
+      var target_of_oscalert = k.EMPTY;
+      var condition_clause = k.EMPTY;
+      if (new ArrayList() {"AlsNeeded","CardiacArrestAlsNeeded","MultAlsHolds"}.Contains(description))
+        {
+        condition_clause = " min_oscalert_peck_order_als <= (select pecking_order from field_situation_impression where description = '" + description + "')";
+        }
+      else if (description == "Trap")
+        {
+        condition_clause = " do_oscalert_for_trap";
+        }
+      else if (description == "AirportAlert")
+        {
+        condition_clause = " do_oscalert_for_airport_alert";
+        }
+      else if (description == "MrtCall")
+        {
+        condition_clause = " do_oscalert_for_mrt";
+        }
+      else if (description == "SarCall")
+        {
+        condition_clause = " do_oscalert_for_sart";
+        }
+      else if (new ArrayList() {"StabbingOrGsw","CardiacArrest","WorkingFire"}.Contains(description))
+        {
+        condition_clause = " FALSE"; // Suppress notifications.  Logging takes place anyway, because it is of interest on the Active Case Board.
+        }
+      else
+        {
+        condition_clause = " min_oscalert_peck_order_general <= (select pecking_order from field_situation_impression where description = '" + description + "')";
+        }
+      Open();
+      var dr = new MySqlCommand("select CONCAT(phone_num,'@',hostname) as sms_target from member join sms_gateway on (sms_gateway.id=member.phone_service_id) where " + condition_clause,connection).ExecuteReader();
+      while (dr.Read())
+        {
+        target_of_oscalert += dr["sms_target"].ToString() + k.COMMA;
+        }
+      dr.Close();
+      Close();
+      return target_of_oscalert.TrimEnd(new char[] {Convert.ToChar(k.COMMA)});
       }
 
     } // end TClass_db_notifications
