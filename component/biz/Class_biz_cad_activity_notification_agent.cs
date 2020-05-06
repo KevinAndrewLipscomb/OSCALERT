13cd71ce54bb30ec1a1195a083213141fff248fb
kevinanlipscomb <kevinandrewlipscomb@vt.edu>
2020-05-06 18:45:39 +0000

    Revised to account for the weird JSON model made on ImageTrend's end.

--- component/biz/Class_biz_cad_activity_notification_agent.cs
+++ component/biz/Class_biz_cad_activity_notification_agent.cs
@@ -24,7 +24,9 @@ namespace Class_biz_cad_activity_notification_agent
       var biz_field_situations = new TClass_biz_field_situations();
       var ss_imagetrendelite = new TClass_ss_imagetrendelite();
       //
+      var address = k.EMPTY;
       var current_incident_num = k.EMPTY;
+      var incident_date_time_initialized = k.EMPTY;
       var nature = k.EMPTY;
       //var saved_incident_num = k.EMPTY; // for use managing nature
       var saved_meta_surge_alert_timestamp_ems = DateTime.MinValue;
@@ -57,35 +59,52 @@ namespace Class_biz_cad_activity_notification_agent
           for (var i = new k.subtype<int>(0,rows.Count); i.val < i.LAST; i.val++)
             {
             var cells = rows[i.val].Columns;
-            if (cells[10].Value.Length > 1) // then there is an incident_date/time_initialized
+            //
+            // Remove the IncidentNumberNotValue cell if it exists in this record.  It only appears in some records.
+            //
+            if (cells.Count == 19)
               {
-              current_incident_num = cells[1].Value;
+              cells.RemoveAt(2);
+              }
+            //
+            address = cells[4].Value;
+            current_incident_num = cells[1].Value;
+            incident_date_time_initialized = cells[9].Value;
+            if(
+                (incident_date_time_initialized.Length > 1) // there is an incident_date/time_initialized
+              &&
+                (address.Length > 1) // there is an address
+              &&
+                (
+                  (current_incident_num[0] == 'E') // this is an EMS incident whose number starts with EMS
+                ||
+                  (current_incident_num[0] == 'F') // this if a fire incident whose number starts with FD
+                )
+              )
+              {
               //if (current_incident_num != saved_incident_num)
               //  {
               //  //
               //  // Determine nature, if supported.
               //  //
               //  }
-              if (current_incident_num[0] == 'E' || current_incident_num[0] == 'F') // starts with "EMS or "FD"
-                {
-                biz_cad_records.Set
-                  (
-                  id:k.EMPTY,
-                  incident_date:(cells[10].Value.Split())[0],
-                  incident_num:current_incident_num,
-                  incident_address:cells[5].Value,
-                  call_sign:cells[7].Value,
-                  time_initialized:(cells[10].Value.Split())[1],
-                  time_of_alarm:(cells[0].Value.Contains(k.SPACE) ? (cells[0].Value.Split())[1] : k.EMPTY),
-                  time_enroute:(cells[11].Value.Contains(k.SPACE) ? (cells[11].Value.Split())[1] : k.EMPTY),
-                  time_on_scene:(cells[17].Value.Contains(k.SPACE) ? (cells[17].Value.Split())[1] : k.EMPTY),
-                  time_transporting:(cells[13].Value.Contains(k.SPACE) ? (cells[13].Value.Split())[1] : k.EMPTY),
-                  time_at_hospital:(cells[14].Value.Contains(k.SPACE) ? (cells[14].Value.Split())[1] : k.EMPTY),
-                  time_available:(cells[15].Value.Contains(k.SPACE) ? (cells[15].Value.Split())[1] : k.EMPTY),
-                  time_downloaded:k.EMPTY,
-                  nature:nature
-                  );
-                }
+              biz_cad_records.Set
+                (
+                id:k.EMPTY,
+                incident_date:(incident_date_time_initialized.Split())[0],
+                incident_num:current_incident_num,
+                incident_address:address,
+                call_sign:cells[6].Value,
+                time_initialized:(incident_date_time_initialized.Split())[1],
+                time_of_alarm:(cells[0].Value.Contains(k.SPACE) ? (cells[0].Value.Split())[1] : k.EMPTY),
+                time_enroute:(cells[10].Value.Contains(k.SPACE) ? (cells[10].Value.Split())[1] : k.EMPTY),
+                time_on_scene:(cells[16].Value.Contains(k.SPACE) ? (cells[16].Value.Split())[1] : k.EMPTY),
+                time_transporting:(cells[12].Value.Contains(k.SPACE) ? (cells[12].Value.Split())[1] : k.EMPTY),
+                time_at_hospital:(cells[13].Value.Contains(k.SPACE) ? (cells[13].Value.Split())[1] : k.EMPTY),
+                time_available:(cells[14].Value.Contains(k.SPACE) ? (cells[14].Value.Split())[1] : k.EMPTY),
+                time_downloaded:k.EMPTY,
+                nature:nature
+                );
               //saved_incident_num = current_incident_num; // for use managing nature
               }
             cells.Clear();
